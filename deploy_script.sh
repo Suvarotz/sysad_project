b43bac944be5e523105475093f5042769fdafca9
Suvarotz <65160363@go.buu.ac.th>
2024-10-30 03:09:02 +0700

    fix

--- deploy_script.sh
+++ deploy_script.sh
@@ -3,9 +3,9 @@
 echo "Starting deployment..."
 
 # ตรวจสอบการ deploy
-if scp -r ./dist suvarot@localhost:3000/var/www/myapp; then
+if scp -r ./dist suvarot@127.0.0.1:/var/www/myapp; then
     echo "Deployment complete!"
 else
     echo "Deployment failed!"
     exit 1
-fi
+fi 
--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+// เสิร์ฟไฟล์ Static จากโฟลเดอร์ที่ deploy
+app.use(express.static('/var/www/myapp'));
+
 // เสิร์ฟไฟล์ HTML
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join('/var/www/myapp', 'index.html'));
 });
 
 app.listen(PORT, () => {
