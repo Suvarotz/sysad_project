e8e11f7faaaf1161ab1283ff8389f989f375d700
Suvarotz <65160363@go.buu.ac.th>
2024-10-30 01:51:34 +0700

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -2,6 +2,11 @@
 
 echo "Starting deployment..."
 
-scp -r ./dist user@your-server-ip:/path/to/deployment-directory
+scp -r ./dist suvarot@192.168.1.100:/var/www/myapp; then
+    echo "Deployment complete!"
+else
+    echo "Deployment failed!"
+    exit 1
+fi
 
 echo "Deployment complete!"
