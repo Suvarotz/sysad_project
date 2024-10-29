142cab5f96945fe850ef6477e85592f4417e4a05
Suvarotz <65160363@go.buu.ac.th>
2024-10-30 05:13:25 +0700

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -17,16 +17,5 @@ docker push "$DOCKER_USERNAME/myapp:latest"
 # รัน Docker container
 container_id=$(docker run -d -p 3000:3000 myapp)
 
-# รอให้แอพพลิเคชั่นเริ่มทำงาน
-sleep 5
-
-# ตรวจสอบสถานะของ container
-if [ "$(docker ps -q -f id=$container_id)" ]; then
-    echo "Container is running."
-else
-    echo "Container is not running."
-    exit 1
-fi
-
 
 echo "Deployment complete!"
