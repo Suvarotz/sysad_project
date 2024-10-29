687cf0941cf7f2f811a7399c85b76a56df78e4de
Suvarotz <65160363@go.buu.ac.th>
2024-10-30 03:50:07 +0700

    deploy

--- deploy_script.sh
+++ deploy_script.sh
@@ -5,7 +5,7 @@ echo "Starting deployment..."
 # สร้าง Docker image
 docker build -t myapp .
 
-# รัน Docker container
-docker run -d -p 192.168.1.100:3000:3000 myapp
+# รัน Docker container (โดยไม่ระบุ IP Address)
+docker run -d -p 3000:3000 myapp
 
 echo "Deployment complete!"
